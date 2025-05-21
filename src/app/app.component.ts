0a96232ea60ef08cb55325033ea8ae1fecb3d215
Annuar Marin <marinfloreannuar@gmail.com>
2025-05-21 09:57:17 -0600

    nuevas Funciones para app.html y .ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IonRouterOutlet } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IonRouterOutlet, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto_git';
+
+  constructor(){
+    console.log('Nuevas funciones');
+    console.warn("Cuidado");
+    console.log("Prueba de conflictos");
+  }
 }
