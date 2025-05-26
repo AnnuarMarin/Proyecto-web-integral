bb087fe384a7f1bf741620a6b210625438880dbf
Annuar Marin <marinfloresannuar@gmail.com>
2025-05-26 09:43:37 -0600

    feat: Diseño del formulario de registro con Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IonRouterOutlet } from "@ionic/angular/standalone";
 import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [IonRouterOutlet, RouterOutlet, LoginComponent],
+  imports: [IonRouterOutlet, RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
