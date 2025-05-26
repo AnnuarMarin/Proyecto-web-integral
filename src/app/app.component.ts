a6239b45aa95eefc3fc57e69d44ed984c0459c36
Annuar Marin <marinfloresannuar@gmail.com>
2025-05-26 09:39:56 -0600

    feat: Diseño del formulario de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IonRouterOutlet } from "@ionic/angular/standalone";
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [IonRouterOutlet, RouterOutlet],
+  imports: [IonRouterOutlet, RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
